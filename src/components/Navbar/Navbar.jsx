5ee5784db9439781e9d113fb03c425839a8de86b
Yusuf Bahaa <yousefbahaa487@gmail.com>
2026-07-24 00:01:14 +0300

    push updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { NavLink, useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie'
 import { useEffect, useState } from "react";
